b19e95be13c6271ce07cc9e338de74f310f4adc6
Stefan Majer <stefan.majer@f-i-ts.de>
2026-05-26 11:47:09 +0200

    Fix path

--- gardener/build.sh
+++ gardener/build.sh
@@ -1,29 +1,33 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
+mkdir work
+cd work
+git clone -b "${BUILD_BRANCH}" https://github.com/metal-stack/gardener.git
+
+cd gardener
+
+REGISTRY=ghcr.io/metal-stack/gardener-builds
 BUILD_BRANCH=${BUILD_BRANCH:-"master"}
+BUILD_SHA=$(git log --pretty=format:'%h' -n 1)
 
-echo "*** Cloning"
 
-DEST_PATH="${GOPATH}/src/github.com/gardener/gardener"
-rm -rf "${DEST_PATH}"
-mkdir -p "${DEST_PATH}"
-git clone -b "${BUILD_BRANCH}" https://github.com/metal-stack/gardener.git "${DEST_PATH}"
+for i in apiserver,gardener-apiserver \
+         controller-manager,gardener-controller-manager \
+         scheduler,gardener-scheduler \
+         gardenlet,gardenlet \
+         node-agent,node-agent \
+         operator,operator \
+         admission-controller,admission-controller; do
+ 
+    TARGET="${i%,*}";
+    IMAGE="${i#*,}";
 
-echo "*** Build and Push"
+    docker build --build-arg BUILDPLATFORM=linux/amd64 \
+        --target "${TARGET}" \
+        --tag "${REGISTRY}/${IMAGE}:${BUILD_BRANCH}" \
+        --tag "${REGISTRY}/${IMAGE}:${BUILD_SHA}" .
 
-cd "${DEST_PATH}"
-export BUILD_SHA=$(git log --pretty=format:'%h' -n 1)
-
-# docker-make --Lint --summary -f "${DOCKERMAKE_APISERVER_FILE_PATH}" --target apiserver
-# docker-make --Lint --summary -f "${DOCKERMAKE_CONTROLLER_MANAGER_FILE_PATH}" --target controller-manager
-# docker-make --Lint --summary -f "${DOCKERMAKE_SCHEDULER_FILE_PATH}" --target scheduler
-# docker-make --Lint --summary -f "${DOCKERMAKE_GARDENLET_FILE_PATH}" --target gardenlet
-# docker-make --Lint --summary -f "${DOCKERMAKE_NODE_AGENT_FILE_PATH}" --target node-agent
-# docker-make --Lint --summary -f "${DOCKERMAKE_OPERATOR_FILE_PATH}" --target operator
-# docker-make --Lint --summary -f "${DOCKERMAKE_ADMISSION_CONTROLLER_FILE_PATH}" --target admission-controller
-
-docker build --build-arg BUILDPLATFORM=linux/amd64 \
-    --target apiserver \
-    --tag ghcr.io/metal-stack/gardener-builds/gardener-apiserver:"${BUILD_BRANCH}" \
-    --tag ghcr.io/metal-stack/gardener-builds/gardener-apiserver:"${BUILD_SHA}" .
+    docker push "${REGISTRY}/${IMAGE}:${BUILD_BRANCH}"
+    docker push "${REGISTRY}/${IMAGE}:${BUILD_SHA}"
+done
